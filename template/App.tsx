54daf49b88dc5faaad3425128117adcd54ddd66d
Muhammad Noman Jelani <noman@Muhammads-MacBook-Pro.local>
2022-10-28 12:11:45 +0500

    Some Style changes on App.tsx also default state of storybook to false is set

--- template/App.tsx
+++ template/App.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, Text, StyleSheet} from 'react-native';
 import StorybookUIRoot from './storybook/index';
 import {USE_STORYBOOK} from './src/common/config';
 
 const App = () => {
- return(<SafeAreaView>
-  <Text>My first template</Text>
-</SafeAreaView>)
-      
-    }
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>My first template</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default USE_STORYBOOK ? StorybookUIRoot : App;
--- template/src/common/config.ts
+++ template/src/common/config.ts
@@ -1,1 +1,1 @@
-export const USE_STORYBOOK = true;
+export const USE_STORYBOOK = false;
